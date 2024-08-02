d7c102db982aedac908355763341dbe4ffe6112a
Clevoh <clevelihasi@gmail.com>
2024-08-02 03:47:32 +0000

    3-app.py

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,39 +1,44 @@
 #!/usr/bin/env python3
-"""Parametrize templates
 """
-from flask import Flask, render_template, request
+Flask app
+"""
+from flask import (
+    Flask,
+    render_template,
+    request
+)
 from flask_babel import Babel
 
 
-class Config:
-    """Configuration for babel
+class Config(object):
     """
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    Configuration for Babel
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
-app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
 @babel.localeselector
 def get_locale():
-    """Retrieve request locale
+    """
+    Select and return best language match based on supported languages
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
-# babel.init_app(app, locale_selector=get_locale)
 
-
-@app.route('/')
-def say_hello():
-    """Handles route by rendering 3-index.html
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """
+    Handles / route
     """
     return render_template('3-index.html')
 
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="3030")
+if __name__ == "__main__":
+    app.run(port="5000", host="0.0.0.0", debug=True)
